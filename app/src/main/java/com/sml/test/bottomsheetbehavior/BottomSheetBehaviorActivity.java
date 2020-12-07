07cdb68a86bee6ae837f563faca8b040ff4ea0fe
sunmenglong <13121980161@163.com>
2020-12-07 15:58:15 +0800

    更新

--- app/src/main/java/com/sml/test/bottomsheetbehavior/BottomSheetBehaviorActivity.java
+++ app/src/main/java/com/sml/test/bottomsheetbehavior/BottomSheetBehaviorActivity.java
@@ -1,16 +1,48 @@
 package com.sml.test.bottomsheetbehavior;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.sml.test.R;
 
-public class BottomSheetBehaviorActivity extends AppCompatActivity {
+public class BottomSheetBehaviorActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_bottom_sheet_behavior);
+
+        final BottomSheetBehavior bottomSheetBehavior=BottomSheetBehavior.from(findViewById(R.id.design_bottom_sheet1));
+        //设置默认先隐藏
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //根据状态不同显示隐藏
+                if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                } else if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                }
+            }
+        });
+        //设置监听事件
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                //拖动
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                //状态变化
+            }
+        });
     }
 }
